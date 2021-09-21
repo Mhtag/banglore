e4d4cb14077ff49fe9ceef856789f5bb6599b5ca
Mohit Agarwal <mohit.agarwal44@gmail.com>
2021-09-21 16:04:21 +0530

    'left for testing'

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from werkzeug.wrappers import response
 import util
 
 app = Flask(__name__)
@@ -11,6 +12,20 @@ def get_location_names():
     response.headers.add("Access-Control-Allow_Origin", 'r')
 
     return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form["total_sqft"])
+    location = request.form("location")
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- server/util.py
+++ server/util.py
@@ -1,9 +1,26 @@
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index([__locations.lowe()])
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
 
 def get_location_names():
     return __locations
@@ -17,6 +34,8 @@ def load_saved_artifacts():
         __data_columns = json.load(f)["data_columns"]
         __locations = __data_columns[3:]
 
+    
+    global __model
     with open("D:/git/banglore/server/artifacts/banglore_home_prices_model.pickle", 'rb') as f:
         __model = pickle.load(f)
     print('Loading the artifacts done!')
@@ -24,3 +43,6 @@ def load_saved_artifacts():
 if __name__ == "__main__":
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st phase jp nagar', '1000','3','2'))
+    print(get_estimated_price('9th phase jp nagar', '1500','4','3'))
+    print(get_estimated_price('dodda nekkundi', '700','2','2'))
